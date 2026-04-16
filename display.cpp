0a615a64a21eae85b706e2ebbec5c6c39b68d9fe
Boomija Balasubramanian <boomijabalasubramanian03@gmail.com>
2026-04-16 22:03:34 +0530

    Add display.cpp to print a tongue twister

---
+++ display.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   cout << "she sells sea shells by the seashore" << endl;
+   cout << "she" << endl << "sells" << endl << "sea" << endl << "shells" << endl << "by" << endl << "the" << endl << "seashore" << endl;
+   cout << "| she |" << endl << "| sells |" << endl << "| sea |" << endl << "| shells |" << endl << "| by |" << endl << "| the |" << endl << "| seashore |" <<  endl;
+   return 0;
+}
